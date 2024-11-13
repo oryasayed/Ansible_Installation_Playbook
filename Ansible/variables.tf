17efbfdff4e9d1737985290bdf1175a17b287e03
oryasayed <94091919+oryasayed@users.noreply.github.com>
2024-11-13 16:10:19 -0500

    Add files via upload

---
+++ Ansible/variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {}
+variable "instance_type" {}
+variable "tag_name" {}
+variable "subnet_id" {}
+variable "sg_for_Ansible" {}
+variable "enable_public_ip_address" {}
+variable "user_data_install_Ansible" {}
