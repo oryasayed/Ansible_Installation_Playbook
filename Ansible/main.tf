34b83cdb8f67aa8468672cec1aa21dd1e21610da
oryasayed <94091919+oryasayed@users.noreply.github.com>
2024-11-13 16:13:21 -0500

    Add files via upload

--- Ansible/main.tf
+++ Ansible/main.tf
@@ -1,19 +1,31 @@
-resource "aws_instance" "Ansible" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name = "ec2_keypair"
-
-  tags = {
-    Name = var.tag_name
-  }
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = var.sg_for_Ansible
-  associate_public_ip_address = var.enable_public_ip_address
-
-  user_data = var.user_data_install_Ansible
-
-  metadata_options {
-    http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
-    http_tokens   = "required" # Require the use of IMDSv2 tokens
-  }
+provider "aws" {
+  region = var.region
 }
+
+module "networking" {
+  source               = "./networking"
+  vpc_cidr             = var.vpc_cidr
+  vpc_name             = var.vpc_name
+  cidr_public_subnet   = var.cidr_public_subnet
+  us_availability_zone = var.us_availability_zone
+  cidr_private_subnet  = var.cidr_private_subnet
+}
+
+module "security_group" {
+  source              = "./security-groups"
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  vpc_id              = module.networking.project_vpc_id
+  ec2_Ansible_sg_name = "Allow port 8080 for Ansible"
+}
+
+module "Ansible" {
+  source                    = "./Ansible"
+  ami_id                    = var.ec2_ami_id
+  instance_type             = "t2.medium"
+  tag_name                  = "Ansible"
+  subnet_id                 = tolist(module.networking.project_public_subnets)[0]
+  sg_for_Ansible           = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_Ansible_port_8080]
+  enable_public_ip_address  = true
+  user_data_install_Ansible = templatefile("./Ansible-runner-script/Ansible-installer.sh", {})
+}
+
