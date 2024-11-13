89b36220809f1e15247d60ea68e48ea23d7084fd
oryasayed <94091919+oryasayed@users.noreply.github.com>
2024-11-13 14:16:24 -0700

    Delete Ansible/variables.tf

--- Ansible/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "tag_name" {}
-variable "subnet_id" {}
-variable "sg_for_Ansible" {}
-variable "enable_public_ip_address" {}
-variable "user_data_install_Ansible" {}
